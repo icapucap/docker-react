e66e59c184f4713b6eb8ecdf3b4cebfeb721af05
icapucap <s.shidharth@hotmail.com>
2020-01-07 17:31:09 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          Hey there!
           Edit <code>src/App.js</code> and save to reload.
+          Happy Learning!
         </p>
         <a
           className="App-link"
